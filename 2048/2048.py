202d9a4e5baedd58a77d139fec147118b71dc78d
pjotr145 <52573376+pjotr145@users.noreply.github.com>
2020-07-25 20:53:41 +0200

    little better following pep8

--- 2048/2048.py
+++ 2048/2048.py
@@ -18,6 +18,7 @@ max_moves = 3000
 # Max number of games
 max_games = 1000
 
+
 def play_one_game(spel, nn):
     count_moves = 0
     one_game = {}
@@ -67,6 +68,7 @@ def play_one_game(spel, nn):
         one_game[count_moves] = one_move
     return max(spel.board), one_game
 
+
 def random_direction():
     ''' Simulates a player by chosing randomly in wich direction to move '''
     rand_direction = random()
@@ -85,7 +87,7 @@ nn = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
 
 if __name__ == "__main__":
     counting_total_games = []
-    while len(counting_total_games) < 10:
+    while len(counting_total_games) < 3:
         nmr_games = 0
         max_val = 0
         while max_val < 512:
@@ -94,7 +96,7 @@ if __name__ == "__main__":
             high_game_value, game_steps = play_one_game(spel, nn)
             if high_game_value > max_val:
                 max_val = high_game_value
-            nmr_games +=1
+            nmr_games += 1
         print(spel)
         print("Aantal spellen: {}".format(nmr_games))
         counting_total_games += [nmr_games]
